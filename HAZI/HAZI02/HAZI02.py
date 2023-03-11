165f846d1177c6d251308ef1b6c97c84fead8ab6
Bene Máté <mate.bene0502@gmail.com>
2023-03-11 19:02:46 +0100

    Hazi 2 fix

--- HAZI/HAZI02/HAZI02.py
+++ HAZI/HAZI02/HAZI02.py
@@ -2,7 +2,15 @@
 import numpy as np
 
 # %%
-# Írj egy olyan fügvényt, ami megfordítja egy 2d array oszlopait
+#FONTOS!!!
+
+# CSAK OTT LEHET HASZNÁLNI FOR LOOP-OT AHOL A FELADAT KÜLÖN KÉRI!
+# [1,2,3,4] --> ezek az értékek np.array-ek. Ahol listát kérek paraméterként ott külön ki fogom emelni!
+# Ha végeztél a feladatokkal, akkor notebook-ot alakítsd át .py.
+# A FÁJLBAN CSAK A FÜGGVÉNYEK LEGYENEK! (KOMMENTEK MARADHATNAK)
+
+# %%
+# Írj egy olyan fügvényt, ami megfordítja egy 2d array oszlopait. Bemenetként egy array-t vár.
 # Be: [[1,2],[3,4]]
 # Ki: [[2,1],[4,3]]
 # column_swap()
@@ -13,7 +21,7 @@ def column_swap(array: np.array) -> np.array:
     return array
 
 # %%
-#Készíts egy olyan függvényt ami összehasonlít két array-t és adjon vissza egy array-ben, hogy hol egyenlőek 
+# Készíts egy olyan függvényt ami összehasonlít két array-t és adjon vissza egy array-ben, hogy hol egyenlőek 
 # Pl Be: [7,8,9], [9,8,7] 
 # Ki: [1]
 # compare_two_array()
@@ -21,14 +29,14 @@ def column_swap(array: np.array) -> np.array:
 
 # %%
 def compare_two_array(array1: np.array, array2: np.array) -> np.array:
-    return np.where(array1 == array2)[0]
+    return np.where(np.equal(array1, array2))[0]
 
 # %%
-#Készíts egy olyan függvényt, ami vissza adja a megadott array dimenzióit:
+# Készíts egy olyan függvényt, ami vissza adja string-ként a megadott array dimenzióit:
 # Be: [[1,2,3], [4,5,6]]
 # Ki: "sor: 2, oszlop: 3, melyseg: 1"
 # get_array_shape()
-# 3D-vel még műküdnie kell!
+# 3D-vel még műküdnie kell!, 
 
 # %%
 import numpy as np
@@ -40,8 +48,9 @@ def get_array_shape(array: np.array) -> np.array:
     return f"sor: {sor}, oszlop: {oszlop}, melyseg: {melyseg}"
 
 # %%
-# Készíts egy olyan függvényt, aminek segítségével elő tudod állítani egy neurális hálózat tanításához szükséges Y-okat egy numpy array-ből. 
-#Bementként add meg az array-t, illetve hogy mennyi class-od van. Kimenetként pedig adjon vissza egy 2d array-t, ahol a sorok az egyes elemek. Minden nullákkal teli legyen és csak ott álljon egyes, ahol a bementi tömb megjelöli
+# Készíts egy olyan függvényt, aminek segítségével elő tudod állítani egy neurális hálózat tanításához szükséges pred-et egy numpy array-ből. 
+# Bementként add meg az array-t, illetve hogy mennyi class-od van. Kimenetként pedig adjon vissza egy 2d array-t, ahol a sorok az egyes elemek. Minden nullákkal teli legyen és csak ott álljon egyes, ahol a bementi tömb megjelöli. 
+# Pl. ha 1 van a bemeneten és 4 classod van, akkor az adott sorban az array-ban a [1] helyen álljon egy 1-es, a többi helyen pedig 0.
 # Be: [1, 2, 0, 3], 4
 # Ki: [[0,1,0,0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1]]
 # encode_Y()
@@ -69,8 +78,8 @@ def decode_Y(array: np.array) -> np.array:
     return result
 
 # %%
-# Készíts egy olyan függvényt, ami képes kiértékelni egy neurális háló eredményét! Bemenetként egy listát és egy array-t és adja vissza a legvalószínübb element a listából.
-# Be: ['alma', 'körte', 'szilva'], [0.2, 0.2, 0.6]. 
+# Készíts egy olyan függvényt, ami képes kiértékelni egy neurális háló eredményét! Bemenetként egy listát és egy array-t és adja vissza azt az elemet, aminek a legnagyobb a valószínüsége(értéke) a listából.
+# Be: ['alma', 'körte', 'szilva'], [0.2, 0.2, 0.6]. # Az ['alma', 'körte', 'szilva'] egy lista!
 # Ki: 'szilva'
 # eval_classification()
 
@@ -82,7 +91,7 @@ def eval_classification(options: np.array, probabilities: np.array) -> str:
 # Készíts egy olyan függvényt, ahol az 1D array-ben a páratlan számokat -1-re cseréli
 # Be: [1,2,3,4,5,6]
 # Ki: [-1,2,-1,4,-1,6]
-# repalce_odd_numbers()
+# replace_odd_numbers()
 
 # %%
 def replace_odd_numbers(array: np.array) -> np.array:
@@ -100,7 +109,7 @@ def replace_by_value(array: np.array, threshold: int) -> np.array:
     return np.where(array < threshold, -1, 1)
 
 # %%
-# Készítsd egy olyan függvényt, ami az array értékeit összeszorozza és az eredmény visszaadja
+# Készíts egy olyan függvényt, ami egy array értékeit összeszorozza és az eredményt visszaadja
 # Be: [1,2,3,4]
 # Ki: 24
 # array_multi()
@@ -111,7 +120,7 @@ def array_multi(array: np.array) -> int:
     return np.prod(array)
 
 # %%
-# Készítsd egy olyan függvényt, ami a 2D array értékeit összeszorozza és egy olyan array-el tér vissza, aminek az elemei a soroknak a szorzata
+# Készíts egy olyan függvényt, ami egy 2D array értékeit összeszorozza és egy olyan array-el tér vissza, aminek az elemei a soroknak a szorzata
 # Be: [[1, 2], [3, 4]]
 # Ki: [2, 12]
 # array_multi_2d()
@@ -134,8 +143,11 @@ def add_border(array: np.array) -> np.array:
     return result
 
 # %%
-# Készíts egy olyan függvényt ami két dátum között felsorolja az összes napot.
-# Be: '2023-03', '2023-04'
+# A KÖTVETKEZŐ FELADATOKHOZ NÉZZÉTEK MEG A NUMPY DATA TYPE-JÁT!
+
+# %%
+# Készíts egy olyan függvényt ami két dátum között felsorolja az összes napot és ezt adja vissza egy numpy array-ben. A fgv ként str vár paraméterként 'YYYY-MM' formában.
+# Be: '2023-03', '2023-04'  # mind a kettő paraméter str.
 # Ki: ['2023-03-01', '2023-03-02', .. , '2023-03-31',]
 # list_days()
 
@@ -144,25 +156,25 @@ def list_days(date1, date2) -> np.array:
     return np.arange(date1, date2, dtype='datetime64[D]')
 
 # %%
-# Írj egy fügvényt ami vissza adja az aktuális dátumot az alábbi formában: YYYY-MM-DD
+# Írj egy fügvényt ami vissza adja az aktuális dátumot az alábbi formában: YYYY-MM-DD. Térjen vissza egy 'numpy.datetime64' típussal.
 # Be:
-# Ki: 2017-03-24 
+# Ki: 2017-03-24
+# get_act_date()
 
 # %%
 def get_act_date() -> np.datetime64:
     return np.datetime64('today', 'D')
 
 # %%
-# Írj egy olyan függvényt ami visszadja, hogy mennyi másodperc telt el 1970 január 01. 00:00:00 óta.
+# Írj egy olyan függvényt ami visszadja, hogy mennyi másodperc telt el 1970 január 01. 00:02:00 óta. Int-el térjen vissza
 # Be: 
 # Ki: másodpercben az idó, int-é kasztolva
 # sec_from_1970()
 
-
 # %%
 def sec_from_1970() -> int:
     current = np.datetime64('today', 'D')
-    old = np.datetime64("1970-01-01")
+    old = np.datetime64("1970-01-01T00:02:00")
     return int((current - old) / np.timedelta64(1, 's'))
 
 
